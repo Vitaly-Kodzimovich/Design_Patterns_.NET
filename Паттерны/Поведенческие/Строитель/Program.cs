0037287d5de48422b0a5c47bc0d33356f213efa6
Vitaly-Kodzimovich <vit20212120@gmail.com>
2024-03-02 20:59:47 +0300

    Added Builder Pattern

---
+++ Паттерны/Поведенческие/Строитель/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Builder
+{
+    public interface IBuilder
+    {
+        void BuildAttic();
+        
+        void BuildResidentalFloor();
+        
+        void BuildBasement();
+    }
+    
+    public class ConcreteBuilder : IBuilder
+    {
+        private House _House = new House();
+        
+        public ConcreteBuilder()
+        {
+            this.Reset();
+        }
+        
+        public void Reset()
+        {
+            this._House = new House();
+        }
+        
+        public void BuildAttic()
+        {
+            this._House.Add("Attic");
+        }
+        
+        public void BuildResidentalFloor()
+        {
+            this._House.Add("Residental Floor");
+        }
+        
+        public void BuildBasement()
+        {
+            this._House.Add("Basement");
+        }
+        
+        public House GetHouse()
+        {
+            House result = this._House;
+
+            this.Reset();
+
+            return result;
+        }
+    }
+    
+    public class House
+    {
+        private List<object> _parts = new List<object>();
+        
+        public void Add(string part)
+        {
+            this._parts.Add(part);
+        }
+        
+        public string ListParts()
+        {
+            string str = string.Empty;
+
+            for (int i = 0; i < this._parts.Count; i++)
+            {
+                str += this._parts[i] + ", ";
+            }
+
+            str = str.Remove(str.Length - 2); // removing last ", "
+
+            return "House parts: " + str + "\n";
+        }
+    }
+    
+    public class Director
+    {
+        private IBuilder _builder = null!;
+        
+        public IBuilder Builder
+        {
+            set { _builder = value; } 
+        }
+        
+        public void BuildMinimalViableHouse()
+        {
+            this._builder.BuildResidentalFloor();
+        }
+        
+        public void BuildFullFeaturedHouse()
+        {
+            this._builder.BuildAttic();
+            this._builder.BuildResidentalFloor();
+            this._builder.BuildBasement();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var director = new Director();
+            var builder = new ConcreteBuilder();
+            director.Builder = builder;
+            
+            Console.WriteLine("Cheap House:");
+            director.BuildMinimalViableHouse();
+            Console.WriteLine(builder.GetHouse().ListParts());
+
+            Console.WriteLine("Standard House:");
+            director.BuildFullFeaturedHouse();
+            Console.WriteLine(builder.GetHouse().ListParts());
+
+            Console.WriteLine("Custom House:");
+            builder.BuildAttic();
+            builder.BuildResidentalFloor();
+            builder.BuildResidentalFloor();
+            builder.BuildBasement();
+            Console.Write(builder.GetHouse().ListParts());
+        }
+    }
+}
