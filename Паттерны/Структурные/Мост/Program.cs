7c9bc74f06e26619b93a1f22850485cceb3927d8
Vitaly-Kodzimovich <vit20212120@gmail.com>
2024-03-05 18:41:56 +0300

    Added Bridge Pattern

---
+++ Паттерны/Структурные/Мост/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Bridge
+{
+    public interface IOperatingSystem
+    {
+        string SystemName();
+    }
+
+
+
+
+    class BasicProgram
+    {
+        protected IOperatingSystem _operatingSystem;
+        
+        public BasicProgram(IOperatingSystem operatingSystem)
+        {
+            this._operatingSystem = operatingSystem;
+        }
+        
+        public virtual string StartPage()
+        {
+            return "-You are using basic version of our program\n" +
+                   "Current Operating System: " +
+                   _operatingSystem.SystemName() + "\n\n";
+        }
+    }
+
+    class ExtendedProgram : BasicProgram
+    {
+        public ExtendedProgram(IOperatingSystem operatingSystem) : base(operatingSystem)
+        {
+        }
+        
+        public override string StartPage()
+        {
+            return "-You are using extended version of our program\n" +
+                   "-New features have been added\n"+
+                   "Current Operating System: " +
+                    base._operatingSystem.SystemName() + "\n\n";
+        }
+    }
+
+
+
+    class WindowsOS : IOperatingSystem
+    {
+        public string SystemName()
+        {
+            return "Windows";
+        }
+    }
+
+    class LinuxOS : IOperatingSystem
+    {
+        public string SystemName()
+        {
+            return "Linux";
+        }
+    }
+
+    class MacOS : IOperatingSystem
+    {
+        public string SystemName()
+        {
+            return "MacOS";
+        }
+    }
+
+
+
+    class Client
+    {
+        public void ClientCode(BasicProgram program)
+        {
+            Console.Write(program.StartPage());
+        }
+    }
+    
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+
+            BasicProgram program1;
+            program1 = new BasicProgram(new WindowsOS());
+            client.ClientCode(program1);
+            
+            ExtendedProgram program2 = new ExtendedProgram(new LinuxOS());
+            client.ClientCode(program2);
+
+            BasicProgram program3;
+            program3 = new ExtendedProgram(new MacOS());
+            client.ClientCode(program3);
+        }
+    }
+}
