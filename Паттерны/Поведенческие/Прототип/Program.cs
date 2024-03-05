8270ce8c6c9b669ecba2543fb48d8910733394df
Vitaly-Kodzimovich <vit20212120@gmail.com>
2024-03-05 16:19:16 +0300

    Added Prototype pattern

---
+++ Паттерны/Поведенческие/Прототип/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Prototype
+{
+    public class Person
+    {
+        public int Age;
+        public DateTime BirthDate;
+        public string Name = null!;
+        public IdInfo IdInfo = null!;
+
+        public Person ShallowCopy()
+        {
+            return (Person) this.MemberwiseClone();
+        }
+
+        public Person DeepCopy()
+        {
+            Person clone = (Person) this.MemberwiseClone();
+            clone.IdInfo = new IdInfo(IdInfo.IdNumber);
+            clone.Name = Name;
+            return clone;
+        }
+    }
+
+    public class IdInfo
+    {
+        public int IdNumber;
+
+        public IdInfo(int idNumber)
+        {
+            this.IdNumber = idNumber;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Age = 42;
+            p1.BirthDate = Convert.ToDateTime("1999-01-01");
+            p1.Name = "Jack Daniels";
+            p1.IdInfo = new IdInfo(1);
+
+            // Выполнить поверхностное копирование p1 и присвоить её p2.
+            Person p2 = p1.ShallowCopy();
+            // Сделать глубокую копию p1 и присвоить её p3.
+            Person p3 = p1.DeepCopy();
+
+            // Вывести значения p1, p2 и p3.
+            Console.WriteLine("Original values of p1, p2, p3:");
+            Console.WriteLine("   p1 instance values: ");
+            DisplayValues(p1);
+            Console.WriteLine("   p2 instance values:");
+            DisplayValues(p2);
+            Console.WriteLine("   p3 instance values:");
+            DisplayValues(p3);
+
+            // Изменить значение свойств p1 и отобразить значения p1, p2 и p3.
+            p1.Age = 32;
+            p1.BirthDate = Convert.ToDateTime("2007-01-01");
+            p1.Name = "Frank";
+            p1.IdInfo.IdNumber = 7878;
+            Console.WriteLine("\nValues of p1, p2 and p3 after changes to p1:");
+            Console.WriteLine("   p1 instance values: ");
+            DisplayValues(p1);
+            Console.WriteLine("   p2 instance values (reference values have changed):");
+            DisplayValues(p2);
+            Console.WriteLine("   p3 instance values (everything was kept the same):");
+            DisplayValues(p3);
+            // В итоге, при поверхностном копировании, p1.Id такое же как и у p2.Id
+            // При глубоком копировании, p3 получило свой собственный Id
+        }
+
+        public static void DisplayValues(Person p)
+        {
+            Console.WriteLine("      Name: {0:s}, Age: {1:d}, BirthDate: {2:MM/dd/yyyy}",
+                p.Name, p.Age, p.BirthDate);
+            Console.WriteLine("      ID#: {0:d}", p.IdInfo.IdNumber);
+        }
+    }
+}
