2665b0979ce5f3ae2645916a9d29353900a1dba5
Vitaly-Kodzimovich <vit20212120@gmail.com>
2024-03-11 15:43:40 +0300

    Added Memento Pattern

---
+++ Паттерны/Поведенческие/Снимок/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+
+namespace Memento
+{
+    class Originator
+    {
+        private string _state;
+
+        public Originator(string state)
+        {
+            this._state = state;
+            Console.WriteLine("Originator: My initial state is: " + state);
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Originator: I'm doing something important.");
+            this._state = this.GenerateRandomString(30);
+            Console.WriteLine($"Originator: and my state has changed to: {_state}");
+        }
+
+        private string GenerateRandomString(int length = 10)
+        {
+            string allowedSymbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string result = string.Empty;
+
+            while (length > 0)
+            {
+                result += allowedSymbols[new Random().Next(0, allowedSymbols.Length)];
+
+                Thread.Sleep(12);
+
+                length--;
+            }
+
+            return result;
+        }
+
+        public IMemento Save()
+        {
+            return new ConcreteMemento(this._state);
+        }
+
+        public void Restore(IMemento memento)
+        {
+            if (!(memento is ConcreteMemento))
+            {
+                throw new Exception("Unknown memento class " + memento.ToString());
+            }
+
+            this._state = memento.GetState();
+            Console.Write($"Originator: My state has changed to: {_state}");
+        }
+    }
+
+
+    public interface IMemento
+    {
+        string GetName();
+
+        string GetState();
+
+        DateTime GetDate();
+    }
+
+    class ConcreteMemento : IMemento
+    {
+        private string _state;
+
+        private DateTime _date;
+
+        public ConcreteMemento(string state)
+        {
+            this._state = state;
+            this._date = DateTime.Now;
+        }
+
+        public string GetState()
+        {
+            return this._state;
+        }
+        
+        public string GetName()
+        {
+            return $"{this._date} / ({this._state.Substring(0, 9)})...";
+        }
+
+        public DateTime GetDate()
+        {
+            return this._date;
+        }
+    }
+
+    class Caretaker
+    {
+        private List<IMemento> _mementos = new List<IMemento>();
+
+        private Originator _originator = null!;
+
+        public Caretaker(Originator originator)
+        {
+            this._originator = originator;
+        }
+
+        public void Backup()
+        {
+            Console.WriteLine("\nCaretaker: Saving Originator's state...");
+            this._mementos.Add(this._originator.Save());
+        }
+
+        public void Undo()
+        {
+            if (this._mementos.Count == 0)
+            {
+                return;
+            }
+
+            var memento = this._mementos.Last();
+            this._mementos.Remove(memento);
+
+            Console.WriteLine("Caretaker: Restoring state to: " + memento.GetName());
+
+            try
+            {
+                this._originator.Restore(memento);
+            }
+            catch (Exception)
+            {
+                this.Undo();
+            }
+        }
+
+        public void ShowHistory()
+        {
+            Console.WriteLine("Caretaker: Here's the list of mementos:");
+
+            foreach (var memento in this._mementos)
+            {
+                Console.WriteLine(memento.GetName());
+            }
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Originator originator = new Originator("Super-duper-super-puper-super.");
+            Caretaker caretaker = new Caretaker(originator);
+
+            caretaker.Backup();
+            originator.DoSomething();
+
+            caretaker.Backup();
+            originator.DoSomething();
+
+            caretaker.Backup();
+            originator.DoSomething();
+
+            Console.WriteLine();
+            caretaker.ShowHistory();
+
+            Console.WriteLine("\nClient: Now, let's rollback!\n");
+            caretaker.Undo();
+
+            Console.WriteLine("\n\nClient: Once more!\n");
+            caretaker.Undo();
+
+            Console.WriteLine();
+        }
+    }
+}
