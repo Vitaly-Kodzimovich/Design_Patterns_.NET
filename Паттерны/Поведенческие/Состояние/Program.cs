276eac601c278b60584eb00c7cd7b2c52a99ae03
Vitaly-Kodzimovich <vit20212120@gmail.com>
2024-03-11 15:56:11 +0300

    Added State Pattern

---
+++ Паттерны/Поведенческие/Состояние/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace State
+{
+    class Context
+    {
+        private State _state = null!;
+
+        public Context(State state)
+        {
+            this.TransitionTo(state);
+        }
+
+        public void TransitionTo(State state)
+        {
+            Console.WriteLine($"Context: Transition to {state.GetType().Name}.");
+            this._state = state;
+            this._state.SetContext(this);
+        }
+
+        public void Request1()
+        {
+            this._state.Handle1();
+        }
+
+        public void Request2()
+        {
+            this._state.Handle2();
+        }
+    }
+    
+    abstract class State
+    {
+        protected Context _context = null!;
+
+        public void SetContext(Context context)
+        {
+            this._context = context;
+        }
+
+        public abstract void Handle1();
+
+        public abstract void Handle2();
+    }
+
+    class ConcreteStateA : State
+    {
+        public override void Handle1()
+        {
+            Console.WriteLine("ConcreteStateA handles request1.");
+            Console.WriteLine("ConcreteStateA wants to change the state of the context.");
+            this._context.TransitionTo(new ConcreteStateB());
+        }
+
+        public override void Handle2()
+        {
+            Console.WriteLine("ConcreteStateA handles request2.");
+        }
+    }
+
+    class ConcreteStateB : State
+    {
+        public override void Handle1()
+        {
+            Console.Write("ConcreteStateB handles request1.");
+        }
+
+        public override void Handle2()
+        {
+            Console.WriteLine("ConcreteStateB handles request2.");
+            Console.WriteLine("ConcreteStateB wants to change the state of the context.");
+            this._context.TransitionTo(new ConcreteStateA());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new Context(new ConcreteStateA());
+            context.Request1();
+            context.Request2();
+        }
+    }
+}
