97ddad3fdb284dea8437bda7d3b213e8ec635b7d
Vitaly-Kodzimovich <vit20212120@gmail.com>
2024-03-11 15:15:17 +0300

    Added Iterator Pattern

---
+++ Паттерны/Поведенческие/Итератор/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+
+namespace Iterator
+{
+    abstract class Iterator : IEnumerator
+    {
+        object IEnumerator.Current => Current();
+
+        public abstract int Key();
+        
+        public abstract object Current();
+        
+        public abstract bool MoveNext();
+        
+        public abstract void Reset();
+    }
+
+    abstract class IteratorAggregate : IEnumerable
+    {
+        public abstract IEnumerator GetEnumerator();
+    }
+
+
+    class AlphabeticalOrderIterator : Iterator
+    {
+        private WordsCollection _collection;
+        
+        private int _position = -1;
+        
+        private bool _reverse = false;
+
+        public AlphabeticalOrderIterator(WordsCollection collection, bool reverse = false)
+        {
+            this._collection = collection;
+            this._reverse = reverse;
+
+            if (reverse)
+            {
+                this._position = collection.getItems().Count;
+            }
+        }
+        
+        public override object Current()
+        {
+            return this._collection.getItems()[_position];
+        }
+
+        public override int Key()
+        {
+            return this._position;
+        }
+        
+        public override bool MoveNext()
+        {
+            int updatedPosition = this._position + (this._reverse ? -1 : 1);
+
+            if (updatedPosition >= 0 && updatedPosition < this._collection.getItems().Count)
+            {
+                this._position = updatedPosition;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        public override void Reset()
+        {
+            this._position = this._reverse ? this._collection.getItems().Count - 1 : 0;
+        }
+    }
+
+    class WordsCollection : IteratorAggregate
+    {
+        List<string> _collection = new List<string>();
+        
+        bool _direction = false;
+        
+        public void ReverseDirection()
+        {
+            _direction = !_direction;
+        }
+        
+        public List<string> getItems()
+        {
+            return _collection;
+        }
+        
+        public void AddItem(string item)
+        {
+            this._collection.Add(item);
+        }
+        
+        public override IEnumerator GetEnumerator()
+        {
+            return new AlphabeticalOrderIterator(this, _direction);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new WordsCollection();
+            collection.AddItem("First");
+            collection.AddItem("Second");
+            collection.AddItem("Third");
+
+            Console.WriteLine("Straight traversal:");
+
+            foreach (var element in collection)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine("\nReverse traversal:");
+
+            collection.ReverseDirection();
+
+            foreach (var element in collection)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
